6bdad863214067d81e40b83314a6922c1b009e61
deysumi89-stack <dey.sumi89@gmail.com>
2026-08-25 16:32:35 -0600

    fastapi methods

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.param_functions import Body
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "BUILDING FASTAPI"}
+
+@app.get("/posts") # for retrieving data, the http method is GET
+async def get_posts():
+    return {"data": "This is first post"}
+
+@app.post("/createposts") # for creating data, the http method is POST
+async def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new post": f"title: {payload['title']}, content: {payload['content']}"}
